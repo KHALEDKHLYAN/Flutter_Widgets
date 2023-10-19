660d5949875e928e48cd358e078b201cf1e6e028
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-10-19 23:11:17 +0300

    widget

---
+++ lib/list_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyListView extends StatelessWidget {
+  const MyListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ListView',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('ListView'),
+          backgroundColor: Colors.deepOrange,
+      ),
+      ),
+      
+      )
+      ;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:widgets_app_design/row.dart';
+import 'package:widgets_app_design/list_view.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,6 +8,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const RowWidget();
+    return const MyListView();
   }
 }
