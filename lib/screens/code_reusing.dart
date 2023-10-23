98ed6643c3123a0ed45c1d4b90ef2fd54ad9b397
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-10-23 11:07:16 +0300

    Custom widget

--- lib/screens/code_reusing.dart
+++ lib/screens/code_reusing.dart
@@ -13,6 +13,91 @@ class ReusingCode extends StatelessWidget {
           title: const Text('Custom WIdget'),
           backgroundColor: Colors.blueGrey,
         ),
+        body: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ListTile(
+                tileColor: Colors.black26,
+                shape: ContinuousRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    side: const BorderSide(
+                      width: 1.0,
+                      color: Colors.redAccent,
+                    )),
+                leading: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.mouse_outlined),
+                  color: Colors.blue,
+                ),
+                title: const Text(
+                  'Mouse',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold),
+                ),
+                subtitle: const Text('4Cm HPModel'),
+                trailing: IconButton(
+                    onPressed: () {}, icon: const Icon(Icons.shop_outlined)),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ListTile(
+                tileColor: Colors.black26,
+                shape: ContinuousRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    side: const BorderSide(
+                      width: 1.0,
+                      color: Colors.redAccent,
+                    )),
+                leading: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.mouse_outlined),
+                  color: Colors.blue,
+                ),
+                title: const Text(
+                  'Mouse',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold),
+                ),
+                subtitle: const Text('4Cm HPModel'),
+                trailing: IconButton(
+                    onPressed: () {}, icon: const Icon(Icons.shop_outlined)),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ListTile(
+                tileColor: Colors.black26,
+                shape: ContinuousRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    side: const BorderSide(
+                      width: 1.0,
+                      color: Colors.redAccent,
+                    )),
+                leading: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.mouse_outlined),
+                  color: Colors.blue,
+                ),
+                title: const Text(
+                  'Mouse',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold),
+                ),
+                subtitle: const Text('4Cm HPModel'),
+                trailing: IconButton(
+                    onPressed: () {}, icon: const Icon(Icons.shop_outlined)),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
